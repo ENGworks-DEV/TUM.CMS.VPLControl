63f39c589e361e9c936a7927d7a4389eb94822b0
Pablo Derendinger <pderendinger@gmail.com>
2018-10-12 00:45:17 -0300

    relocate using TranformOrigin

--- src/TUM.CMS.VplControl.Test/MainWindow.xaml.cs
+++ src/TUM.CMS.VplControl.Test/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using TUM.CMS.VplControl.Utilities;
 
 namespace TUM.CMS.VplControl.Test
 {
+   
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
@@ -82,29 +83,72 @@ namespace TUM.CMS.VplControl.Test
         }
 
         const double ScaleRate = 1.1;
-        private double actualzoom ;
+        private double actualzoom { get; set; } = 1;
         private void Canvas_MouseWheel(object sender, MouseWheelEventArgs e)
         {
 
             //ScaleT.CenterX = ViewBox.ActualWidth / 2;
             //ScaleT.CenterY = ViewBox.ActualHeight / 2;
-            if (e.Delta > 0 )
+            if (e.Delta > 0 && actualzoom > 0.6)
             {
                 ScaleT.ScaleX /= 1.1;
                 ScaleT.ScaleY /= 1.1;
                 VplControl.Height *= 1.1;
                 VplControl.Width *= 1.1;
+                actualzoom /= 1.1;
+                ScaleT.CenterX = VplControl.Width / 2;
+                ScaleT.CenterY = VplControl.Height / 2;
+                ScaleT.ScaleX =1;
+                ScaleT.ScaleY = 1;
                 
+                foreach (VplElement item in VplControl.NodeCollection)
+                {
+                    var t = VplControl.TransformToDescendant(item);
+                    Point point = Mouse.GetPosition(item);
+                    var p = t.Transform(point);
+                    if (item.Border.MinHeight > 0 && item.Border.Width > 0)
+                    {
+                        //Center of item
+                        var ItemY = (item.Top + item.Border.MinHeight / 2);
+                        var ItemX = (item.Left + item.Border.Width / 2);
+
+                        var middlePoint = new Point(((ItemX - p.X) / 2), ((ItemY - p.Y) / 2));
+                        var renderT = item.RenderTransformOrigin = middlePoint;
+                    }
+                }
             }
 
          
-            else if (e.Delta < 0 )
+            else if (e.Delta < 0 && actualzoom < 1.3)
             {
                 ScaleT.ScaleX *= 1.1;
                 ScaleT.ScaleY *= 1.1;
                 VplControl.Height /= 1.1;
                 VplControl.Width /=  1.1;
+                actualzoom *= 1.1;
+                ScaleT.CenterX = VplControl.Width / 2;
+                ScaleT.CenterY = VplControl.Height / 2;
+                ScaleT.ScaleX = 1;
+                ScaleT.ScaleY = 1;
 
+               
+                foreach (VplElement item in VplControl.NodeCollection)
+                {
+                    var t = VplControl.TransformToDescendant(item);
+                    Point point = Mouse.GetPosition(item);
+                    var p = t.Transform(point);
+                    if (item.Border.MinHeight > 0 && item.Border.Width >0)
+                    {
+                        //Center of item
+                        var ItemY = (item.Top + item.Border.MinHeight / 2);
+                        var ItemX = (item.Left + item.Border.Width / 2);
+
+                        var middlePoint = new Point((ItemX + p.X) / 2, (ItemY + p.Y) / 2);
+                        var renderT = item.RenderTransformOrigin = middlePoint;
+                    }
+
+
+                }
             }
             VplControl.UpdateLayout();
 
@@ -132,6 +176,8 @@ namespace TUM.CMS.VplControl.Test
                 //VplGroupControl.MainVplControl.OpenFile(filePath);
             }
         }
+
+       
 
         private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
